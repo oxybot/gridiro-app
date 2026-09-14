cc2ef49177269e9c5bd26d2e5918f43aaa0a2d07
superfaz <16510828+superfaz@users.noreply.github.com>
2026-09-14 08:17:43 +0000

    Update stylelint config to avoid error with TS 7

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,11 +1,10 @@
 // Copyright (C) 2026 Gridiro
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import type { Config } from "stylelint";
-
+/** @type {import("stylelint").Config} */
 export default {
   extends: ["stylelint-config-standard"],
   rules: {
     "import-notation": "string"
   }
-} satisfies Config;
+};
